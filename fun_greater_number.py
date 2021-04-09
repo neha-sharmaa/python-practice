6021ef9540be49de415800e8ca9ad9edaa0e7fab
neha <nehaabhardwaj@gmail.com>
2021-04-10 00:18:28 +0530

    removed commented codes

--- fun_greater_number.py
+++ fun_greater_number.py
@@ -1,14 +1,4 @@
 def greatest(num1,num2,num3):
-    # if (num1>num2):
-    #     if(num1>num3):
-    #         return num1
-    #     else:
-    #         return num3
-    # else:
-    #     if(num2>num3):
-    #         return num2
-    #     else:
-    #         return num3
     if (num1 >= num2) and (num1 >= num3):
         return num1
     elif (num2 >= num1) and (num2 >= num3):
